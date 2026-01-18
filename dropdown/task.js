70bad6e2e0b9094ab62b1980d0dabd97d3ad8298
AlekseyGarev <garev91@gmail.com>
2026-01-18 21:44:28 +0300

    Add files via upload

---
+++ dropdown/task.js
@@ -0,0 +1,23 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const value = dropdown.querySelector('.dropdown__value');
+    const list = dropdown.querySelector('.dropdown__list');
+    const items = dropdown.querySelectorAll('.dropdown__link');
+
+    value.addEventListener('click', () => {
+        list.classList.toggle('dropdown__list_active');
+    });
+
+    items.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const selectedText = item.textContent.trim();
+            value.textContent = selectedText;
+            list.classList.remove('dropdown__list_active');
+        });
+    });
+
+});
+
